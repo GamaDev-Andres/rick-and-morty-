c44c357fba2616b3c6145bcd28c396aa4225e07f
gama_pipe@hotmail.com <gamadev@LAPTOP-5IO84K7D.localdomain>
2022-07-24 16:25:23 -0500

    add page 404 to routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,18 +3,20 @@ import { Route, Routes } from "react-router-dom";
 
 const Character = lazy(() => import("../pages/Character"));
 const Favoritos = lazy(() => import("../pages/Favoritos"));
-const Characteres = lazy(() => import("../pages/Characteres"));
+const Characters = lazy(() => import("../pages/Characters"));
 
 import Spinner from "../components/Spinner";
+import Page404NotFound from "../pages/Page404NotFound";
 
 const AppRouter = () => {
   return (
     <main className="flex-grow">
       <Suspense fallback={<Spinner fullScreen={true} />}>
         <Routes>
-          <Route path="/" element={<Characteres />} />
+          <Route path="/" element={<Characters />} />
           <Route path="/favoritos" element={<Favoritos />} />
           <Route path="/c/:id" element={<Character />} />
+          <Route path="*" element={<Page404NotFound />} />
         </Routes>
       </Suspense>
     </main>
